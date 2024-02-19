12f8dce51ff8dd9cf545165a7eb23fb9322e4615
Hadeer Salah <hadeersalah127@gmail.com>
2024-02-19 11:01:46 +0200

    practicing multiple forms of styling

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-
+import { styled } from "styled-components"
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
   const [enteredPassword, setEnteredPassword] = useState('');
@@ -13,6 +13,13 @@ export default function AuthInputs() {
     }
   }
 
+  const ControledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-bottom: 1.5rem;
+  `
+
   function handleLogin() {
     setSubmitted(true);
   }
@@ -22,9 +29,9 @@ export default function AuthInputs() {
 
   return (
     <div id="auth-inputs">
-      <div className="controls">
+      <ControledDiv>
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? "invalid" : ""}`}>Email</label>
           <input
             type="email"
             className={emailNotValid ? 'invalid' : undefined}
@@ -32,7 +39,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? "invalid" : ""}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
@@ -41,7 +48,7 @@ export default function AuthInputs() {
             }
           />
         </p>
-      </div>
+      </ControledDiv>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
-import logo from '../assets/logo.png';
-
+import logo from '../../assets/logo.png';
+import styles from "./header.module.css"
 export default function Header() {
   return (
-    <header>
+    <header style={styles.header}>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
